f83abc8b407047a8f871dcfbb73f883ee8465df5
Samadhan2025 <samadhan.birajdar25@gmail.com>
2026-02-10 19:42:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function validEmail(str) {
-  //your JS code here.
+  //your JS code here
+	  if (!str) return false;
+
+  const emailRegex =
+    /^[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*@
+     [a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,3})+$/x;
+
+  return emailRegex.test(str);
+
 }
 
 // Do not change the code below.
